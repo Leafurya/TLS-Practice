87db3b3a0658c497c79d68618dd44c10a1b1c672
Leafurya <46297197+Leafurya@users.noreply.github.com>
2024-01-04 16:34:08 +0900

    java업로드

---
+++ java/TLSClient/src/TLSClient/TLSClient.java
@@ -0,0 +1,41 @@
+package TLSClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
+
+public class TLSClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SocketFactory factory=SSLSocketFactory.getDefault();
+		try {
+			Socket sock=factory.createSocket("192.168.1.70",2025);
+			InputStream input=sock.getInputStream();
+			OutputStream output=sock.getOutputStream();
+			
+			String msg="hello java TLS server!";
+			System.out.println("send: "+msg);
+			byte[] data=new byte[1024];
+			
+			output.write(msg.getBytes());
+			input.read(data);
+			System.out.println("recv: "+new String(data));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
+/*
+ * vm argument에 -Djavax.net.ssl.trustStore=경로 -Djavax.net.ssl.trustStorePassword=비번 추가할 것.
+ * keytool로 서버의 인증서, 서버의 키스토어를 export 한다.
+ * export한 파일을 클라쪽에서 keytool를 이용해 import 한다.
+ * 그럼 됨.
+ */
---
+++ java/TLSServer/src/TLSServer/TLSServer.java
@@ -0,0 +1,56 @@
+package TLSServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
+
+public class TLSServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServerSocketFactory factory=SSLServerSocketFactory.getDefault();
+		ServerSocket servSock=null;
+		
+		try {
+			servSock=factory.createServerSocket(2025);
+			System.out.println("server on at port 2025");
+		}catch(IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		Socket clntSock=null;
+		try {
+			clntSock=servSock.accept();
+			InputStream input=clntSock.getInputStream();
+			OutputStream output=clntSock.getOutputStream();
+			byte[] data=new byte[1024];
+			
+			int recvSize=input.read(data);
+			System.out.println("recvSize: "+recvSize+"\ndata: "+new String(data));
+			output.write(data);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			clntSock.close();
+			servSock.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
+/*
+ * keytool을 이용해 keystore를 생성할 것.
+ * vm argument에 -Djavax.net.ssl.keyStore="keystore 경로" -Djavax.net.ssl.keyStorePassword=비밀번호
+ * 를 추가할 것.
+ * */
